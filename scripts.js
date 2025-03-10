387bce62091b992d47f1c5525941a9ec44a8c83f
Medjai04 <eduardomedjai04@gmail.com>
2025-03-10 16:53:52 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Toggle Sidebar
+const sidebar = document.getElementById('sidebar');
+const openSidebarButton = document.getElementById('open-sidebar');
+const closeSidebarButton = document.getElementById('close-sidebar');
+const mainContent = document.querySelector('.main-content');
+
+openSidebarButton.addEventListener('click', () => {
+  sidebar.classList.add('open');
+  mainContent.classList.add('shifted');
+});
+
+closeSidebarButton.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+  mainContent.classList.remove('shifted');
+});
